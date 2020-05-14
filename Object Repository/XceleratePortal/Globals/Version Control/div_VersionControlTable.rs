52fdcf8004957ac3af68405251a59327ff582441
Aliaksandr Belavus <aliaksandr_belavus@epam.com>
2020-05-14 23:25:22 +0300

    From Windows

---
+++ Object Repository/XceleratePortal/Globals/Version Control/div_VersionControlTable.rs
@@ -0,0 +1,64 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<WebElementEntity>
+   <description></description>
+   <name>div_VersionControlTable</name>
+   <tag></tag>
+   <elementGuidId>768385d4-f38a-4ce1-86b6-eb468ea86063</elementGuidId>
+   <selectorCollection>
+      <entry>
+         <key>XPATH</key>
+         <value>//div[@class='panel panel-default']</value>
+      </entry>
+   </selectorCollection>
+   <selectorMethod>XPATH</selectorMethod>
+   <useRalativeImagePath>false</useRalativeImagePath>
+   <webElementProperties>
+      <isSelected>false</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>tag</name>
+      <type>Main</type>
+      <value>div</value>
+   </webElementProperties>
+   <webElementProperties>
+      <isSelected>false</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>class</name>
+      <type>Main</type>
+      <value>col-md-12 well no-padding-bottom</value>
+   </webElementProperties>
+   <webElementProperties>
+      <isSelected>true</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>text</name>
+      <type>Main</type>
+      <value>Year Scenario Version GLBook Active  Add Version Control </value>
+   </webElementProperties>
+   <webElementProperties>
+      <isSelected>false</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>xpath</name>
+      <type>Main</type>
+      <value>/html[@class=&quot;ng-scope&quot;]/body[1]/div[@class=&quot;maincontent ng-scope&quot;]/ov-version-control[@class=&quot;ng-scope ng-isolate-scope&quot;]/prompt-unsaved-changes[@class=&quot;ng-isolate-scope&quot;]/div[@class=&quot;ov-version-control-s&quot;]/section[1]/div[@class=&quot;col-xs-12&quot;]/div[@class=&quot;col-md-12 well no-padding-bottom&quot;]</value>
+   </webElementProperties>
+   <webElementXpaths>
+      <isSelected>true</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>xpath:neighbor</name>
+      <type>Main</type>
+      <value>(.//*[normalize-space(text()) and normalize-space(.)='Version Control'])[4]/following::div[7]</value>
+   </webElementXpaths>
+   <webElementXpaths>
+      <isSelected>false</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>xpath:neighbor</name>
+      <type>Main</type>
+      <value>(.//*[normalize-space(text()) and normalize-space(.)='Welcome,Aliaksandr Belavus'])[2]/following::div[11]</value>
+   </webElementXpaths>
+   <webElementXpaths>
+      <isSelected>false</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>xpath:position</name>
+      <type>Main</type>
+      <value>//section/div/div</value>
+   </webElementXpaths>
+</WebElementEntity>
--- Scripts/Globals/TC5.6_VersionControlPage/Script1589459486343.groovy
+++ Scripts/Globals/TC5.6_VersionControlPage/Script1589459486343.groovy
@@ -18,3 +18,111 @@ import internal.GlobalVariable as GlobalVariable
 
 CustomKeywords.'xcelerate.EnterPortal.LoginToPortal'()
 
+WebUI.mouseOver(findTestObject('XceleratePortal/Dashboard/div_GlobalsMenu'))
+
+WebUI.click(findTestObject('XceleratePortal/Dashboard/a_VersionControlPageMenu'))
+
+WebUI.waitForPageLoad(GlobalVariable.TimeOut)
+
+WebUI.verifyElementVisible(findTestObject('XceleratePortal/Globals/Version Control/div_VersionsLoadedMessage'))
+
+WebUI.verifyElementVisible(findTestObject('XceleratePortal/Globals/Version Control/img_VersionControlIcon'))
+
+WebUI.verifyElementVisible(findTestObject('XceleratePortal/Globals/Version Control/h2_VersionControl'))
+
+WebUI.verifyElementVisible(findTestObject('XceleratePortal/Globals/Version Control/div_VersionControlTable'))
+
+WebUI.verifyElementVisible(findTestObject('XceleratePortal/Globals/Version Control/th_YearColumn'))
+
+WebUI.verifyElementVisible(findTestObject('XceleratePortal/Globals/Version Control/th_ScenarioColumn'))
+
+WebUI.verifyElementVisible(findTestObject('XceleratePortal/Globals/Version Control/th_VersionColumn'))
+
+WebUI.verifyElementVisible(findTestObject('XceleratePortal/Globals/Version Control/th_GLBookColumn'))
+
+WebUI.verifyElementVisible(findTestObject('XceleratePortal/Globals/Version Control/th_ActiveColumn'))
+
+WebUI.verifyElementVisible(findTestObject('XceleratePortal/Globals/Version Control/button_AddVersionControl'))
+
+WebUI.click(findTestObject('XceleratePortal/Globals/Version Control/button_AddVersionControl'))
+
+//Verify new row
+WebUI.verifyElementVisible(findTestObject('XceleratePortal/Globals/Version Control/select_Year'))
+
+WebUI.verifyElementVisible(findTestObject('XceleratePortal/Globals/Version Control/select_Scenario'))
+
+WebUI.verifyElementVisible(findTestObject('XceleratePortal/Globals/Version Control/select_Version'))
+
+WebUI.verifyElementVisible(findTestObject('XceleratePortal/Globals/Version Control/select_GLBook'))
+
+WebUI.verifyElementVisible(findTestObject('XceleratePortal/Globals/Version Control/label_ActiveCheckbox'))
+
+//Verify that checkbox is checked
+WebUI.verifyElementAttributeValue(findTestObject('XceleratePortal/Globals/Version Control/input_ActiveCheckbox'), 'class', 
+    'ng-pristine ng-untouched ng-valid ng-not-empty', GlobalVariable.TimeOut)
+
+WebUI.verifyElementVisible(findTestObject('XceleratePortal/Globals/Version Control/button_Save'))
+
+WebUI.verifyElementVisible(findTestObject('XceleratePortal/Globals/Version Control/button_Remove'))
+
+//Verify options in textbox
+WebUI.verifyOptionsPresent(findTestObject('XceleratePortal/Globals/Version Control/select_Year'), ['111: 11', '12', '2016: FY2016'
+        , '2017: FY2017', '2018: FY2018', '2019: FY2019', '2020: FY2020', '2021: FY2021', 'gfg: fgf', 'NA: No Fiscal Year'])
+
+//Verify selected option by default
+WebUI.verifyOptionSelectedByLabel(findTestObject('XceleratePortal/Globals/Version Control/select_Year'), '2020: FY2020', 
+    false, GlobalVariable.TimeOut)
+
+WebUI.verifyOptionsPresent(findTestObject('XceleratePortal/Globals/Version Control/select_Scenario'), ['NA: No Scenario'
+        , 'Forecast_Input', 'Forecast_Adjustment', 'Budget_Input', 'Budget_Adjustment', 'Actual', ''])
+
+WebUI.verifyOptionSelectedByLabel(findTestObject('XceleratePortal/Globals/Version Control/select_Scenario'), '', false, 
+    GlobalVariable.TimeOut)
+
+WebUI.verifyOptionsPresent(findTestObject('XceleratePortal/Globals/Version Control/select_Version'), ['V5: Version 5', 'V4: Version 4'
+        , 'V3: Version 3', 'V2: Version 2', 'V1: Version 1', 'NA: No Version', 'Active', ''])
+
+WebUI.verifyOptionSelectedByLabel(findTestObject('XceleratePortal/Globals/Version Control/select_Version'), '', false, GlobalVariable.TimeOut)
+
+WebUI.verifyOptionsPresent(findTestObject('XceleratePortal/Globals/Version Control/select_GLBook'), ['NA: No GL Book', 'ee'
+        , 'Cash', 'Accrual', ''])
+
+WebUI.verifyOptionSelectedByLabel(findTestObject('XceleratePortal/Globals/Version Control/select_GLBook'), '', false, GlobalVariable.TimeOut)
+
+WebUI.click(findTestObject('XceleratePortal/Globals/Version Control/label_ActiveCheckbox'))
+
+//Verify that checkbox is unchecked
+WebUI.verifyElementAttributeValue(findTestObject('XceleratePortal/Globals/Version Control/input_ActiveCheckbox'), 'class', 
+    'ng-untouched ng-valid ng-empty ng-dirty ng-valid-parse', GlobalVariable.TimeOut)
+
+WebUI.click(findTestObject('XceleratePortal/Globals/Version Control/button_Remove'))
+
+//Verify that new row is removed
+WebUI.verifyElementNotPresent(findTestObject('XceleratePortal/Globals/Version Control/select_Year'), GlobalVariable.TimeOut)
+
+WebUI.verifyElementNotPresent(findTestObject('XceleratePortal/Globals/Version Control/select_Scenario'), GlobalVariable.TimeOut)
+
+WebUI.verifyElementNotPresent(findTestObject('XceleratePortal/Globals/Version Control/select_Version'), GlobalVariable.TimeOut)
+
+WebUI.verifyElementNotPresent(findTestObject('XceleratePortal/Globals/Version Control/select_GLBook'), GlobalVariable.TimeOut)
+
+WebUI.verifyElementNotPresent(findTestObject('XceleratePortal/Globals/Version Control/label_ActiveCheckbox'), GlobalVariable.TimeOut)
+
+WebUI.verifyElementNotPresent(findTestObject('XceleratePortal/Globals/Version Control/button_Save'), GlobalVariable.TimeOut)
+
+WebUI.verifyElementNotPresent(findTestObject('XceleratePortal/Globals/Version Control/button_Remove'), GlobalVariable.TimeOut)
+
+WebUI.verifyElementVisible(findTestObject('XceleratePortal/Globals/Version Control/th_YearColumn'))
+
+WebUI.verifyElementVisible(findTestObject('XceleratePortal/Globals/Version Control/th_ScenarioColumn'))
+
+WebUI.verifyElementVisible(findTestObject('XceleratePortal/Globals/Version Control/th_VersionColumn'))
+
+WebUI.verifyElementVisible(findTestObject('XceleratePortal/Globals/Version Control/th_GLBookColumn'))
+
+WebUI.verifyElementVisible(findTestObject('XceleratePortal/Globals/Version Control/th_ActiveColumn'))
+
+WebUI.verifyElementVisible(findTestObject('XceleratePortal/Globals/Version Control/button_AddVersionControl'))
+
+WebUI.closeBrowser()
+
